c2279eb08d1d3cf4cf8524809aa333ad59b5e38b
Dima Bond <neverpooh@gmail.com>
2021-01-28 15:31:04 +0200

    Updates

---
+++ precache-manifest.cb08dbc2e5f915eef4392817c3ed9a29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9110422fb50713ae6a1f05f13614409a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e2790023bd96625d9191",
+    "url": "./static/css/2.9634ee28.chunk.css"
+  },
+  {
+    "revision": "98268c133fbae1d7f1a8",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "e2790023bd96625d9191",
+    "url": "./static/js/2.956cd878.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.956cd878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98268c133fbae1d7f1a8",
+    "url": "./static/js/main.6519f10a.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
